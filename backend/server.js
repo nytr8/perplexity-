899d71e6732039b41818f7b22fdafa76fb158163
Srutidev Barman <srutidevbarman2002@gmail.com>
2026-03-18 17:28:45 +0530

    added socket io connection to backend and frontend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import connectDB from "./src/config/db.js";
 import app from "./src/app.js";
 import { interactiveChat } from "./src/services/ai.service.js";
-
+import http from "http";
+import { initSocket } from "./src/sockets/server.socket.js";
+const httpServer = http.createServer(app);
 connectDB();
-
-interactiveChat();
-app.listen(3000, () => {
-  console.log("connected to port 3000");
+initSocket(httpServer);
+// interactiveChat();
+httpServer.listen(3000, () => {
+  console.log("server running in port 3000");
 });
--- frontend/src/app/features/chat/pages/Dashboard.jsx
+++ frontend/src/app/features/chat/pages/Dashboard.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { useSelector } from "react-redux";
-
+import { useChat } from "../hooks/useChat.js";
+import { useEffect } from "react";
 const Dashboard = () => {
+  const chat = useChat();
+  useEffect(() => {
+    chat.initializeSocketConnection();
+  }, []);
+
   const { user } = useSelector((state) => state.auth);
   console.log(user);
   return <div className="text-white">Dashboard</div>;
