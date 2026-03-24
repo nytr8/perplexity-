95075c9ddcb933649a76a0d8821e6af86ba1d83f
Srutidev Barman <srutidevbarman2002@gmail.com>
2026-03-24 22:12:34 +0530

    added changes

--- frontend/src/app/features/auth/services/auth.api.js
+++ frontend/src/app/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const app = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://perplexity-clone-q32m.onrender.com/",
   withCredentials: true,
 });
 
